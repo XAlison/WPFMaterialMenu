f38305c13e32114017820d0e6236df58e98a0541
pvlms <31069317+pvlms@users.noreply.github.com>
2019-07-11 13:11:12 +0300

    Material Design Based Menu With SubItems Application

---
+++ MaterialMenu/ViewModel/ItemMenu.cs
@@ -0,0 +1,32 @@
+﻿using MaterialDesignThemes.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace MaterialMenu.ViewModel
+{
+  public class ItemMenu
+  {
+    public ItemMenu(string header, List<SubItem> subItems, PackIconKind icon)
+    {
+      Header = header;
+      SubItems = subItems;
+      Icon = icon;
+    }
+
+    public ItemMenu(string header, UserControl screen, PackIconKind icon)
+    {
+      Header = header;
+      Screen = screen;
+      Icon = icon;
+    }
+
+    public string Header { get; set; }
+    public PackIconKind Icon { get; set; }
+    public List<SubItem> SubItems { get; set; }
+    public UserControl Screen { get; set; }
+  }
+}
